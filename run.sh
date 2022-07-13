503f638a38ed50d7b514ffa5b7fe43a77856e127
mkpark2017 <mk.park@unist.ac.kr>
2022-07-13 20:33:42 +0900

    upload

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@ nvidia-docker run -it \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --device=/dev/nvidiactl \
+    --device=/dev/nvidia-uvm \
+    --device=/dev/nvidia0 \
     --name px4_gazebo mk/px4_gazebo
 
 export containerId=$(docker ps -l -q)
